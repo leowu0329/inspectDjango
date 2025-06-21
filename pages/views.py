fb465b264998986a13030b6a7c9acdc49f582d03
leowu0329 <ryowuandjanet@gmail.com>
2025-06-21 22:05:02 +0800

    調整首頁

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-class HomePageView(TemplateView):
+class HomePageView(LoginRequiredMixin, TemplateView):
 	template_name = 'home.html'
+	login_url = 'login'
