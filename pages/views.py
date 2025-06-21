ccd7fd609e56a17f0b4b2e51e3ad1a0eeb645029
leowu0329 <ryowuandjanet@gmail.com>
2025-06-21 22:27:31 +0800

    調整首頁-顯示表格

--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import ListView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Case
 
-class HomePageView(LoginRequiredMixin, TemplateView):
+class HomePageView(LoginRequiredMixin, ListView):
+	model = Case
 	template_name = 'home.html'
+	context_object_name = 'cases'
 	login_url = 'login'
